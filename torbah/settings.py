03a9f9cc57a70e5a468c0c4abaf0426fd21ee62d
HusamAlzain <husamalzain@hotmail.com>
2024-04-15 14:19:55 +0300

    new settings

--- torbah/settings.py
+++ torbah/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -167,4 +167,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'  
 
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/' 
