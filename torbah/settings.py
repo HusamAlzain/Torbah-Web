9862bb8c20f10097530efbb1beec77ae76e34361
HusamAlzain <husamalzain@hotmail.com>
2024-04-15 14:13:14 +0300

    first commit

--- torbah/settings.py
+++ torbah/settings.py
@@ -27,12 +27,12 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f+6prd*q=e05(b%_$fdd&lp!=ib6!zo*)&)r-g0p=fz%49c&iq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -63,6 +63,7 @@ NPM_BIN_PATH = "F:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -151,9 +152,17 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'main/static')
 ]
+STATIC_ROOT = BASE_DIR/"staticfiles"
+# STATICFILES_DIRS = [
+# os.path.join(BASE_DIR, "static")
+# ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'  
 
